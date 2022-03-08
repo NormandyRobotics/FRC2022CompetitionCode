c9db6fbd71b4bb41069e3690778e805a5e51cbd4
Your Name <you@example.com>
2022-03-08 11:24:34 -0600

    final current drive with joysticks change

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -5,17 +5,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.Drivetrain;
+//import frc.robot.subsystems.Drivetrain;
 
 public class DriveWithJoysticks extends CommandBase {
-  public final Drivetrain driveTrain;
+  //public final Drivetrain driveTrain;
 
-  /** Creates a new DriveWithJoysticks. */
-  public DriveWithJoysticks(Drivetrain dt) {
-    driveTrain = dt;
-    addRequirements(driveTrain);
+  /** Creates a new DriveWithJoysticks. 
+   * @return */
+  //public DriveWithJoysticks(Drivetrain dt) {
+    public void DriveWithJoystick() {
+    //driveTrain = dt;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.driveTrain);
+
   }
 
   // Called when the command is initially scheduled.
@@ -29,12 +33,33 @@ public class DriveWithJoysticks extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveTrain.driveWithJoysticks(RobotContainer.driverJoystick);
+    double leftSpeed = (RobotContainer.driverJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_LEFT_Y_AXIS)*Constants.DRIVETRAINSPEED);
+    double rightSpeed = (RobotContainer.driverJoystick.getRawAxis(Constants.DRIVER_JOYSTICK_RIGHT_Y_AXIS)*Constants.DRIVETRAINSPEED);
+    double hSpeed;
+
+    if (RobotContainer.driverJoystick.getRawButton(Constants.DRIVER_SHOULDER_TOP_LEFT))
+    {
+      hSpeed = -1*Constants.H_DRIVE_SPEED;
+    }
+    else if (RobotContainer.driverJoystick.getRawButton(Constants.DRIVER_SHOULDER_TOP_RIGHT))
+    {
+      hSpeed = Constants.H_DRIVE_SPEED;
+    }
+    else
+    {
+      hSpeed = 0;
+    }
+
+    RobotContainer.driveTrain.driveWithJoysticks(leftSpeed, rightSpeed, hSpeed);
+    
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) 
+  {
+    RobotContainer.driveTrain.driveWithJoysticks(0,0,0);
+  }
 
   // Returns true when the command should end.
   @Override
